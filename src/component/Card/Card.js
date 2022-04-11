e46c5e6b1485989bbe7dd4168f64dfae95b929e4
owakeelAhmmed <mdowakeelahmed5678@gmail.com>
2022-04-11 16:52:37 +0600

    Review Done

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -5,11 +5,20 @@ const Card = (props) => {
     console.log(props);
     const { image, name, message, rate} = props.card;
     return (
-        <div className="card bg-red-500 p-4 text-center">
-            <img src={image} alt="" />
-            <h1>Name: {name}</h1>
-            <p>{message}</p>
-            <span className='flex '> <FcRating/> {rate}</span>
+        <div className="card bg-red-500 p-4 rounded-md text-center">
+            <div className="p-4">
+
+                <div className="ml-7">
+                    <img src={image} alt="" />
+                </div>
+
+                <div className='mt-4'>
+                    <h1 className='font-bold'>Name: {name}</h1>
+                    <p>{message}</p>
+                    <span className='flex items-center '> <FcRating/> {rate}</span>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import Customlink from '../Customlink/Customlink';
 import img1 from '../images/headphones.png';
+import Reviews from '../Reviews/Reviews';
 import './Home.css'
 const Home = () => {
     return (
-        <div className="flex ">
+        <section>
+            <div className="flex header-style">
             <div className=" w-6/12 h-7">
                 <img src={img1}/>
             </div>
             <div className="p-7">
                 <h1 className="text-4xl text-slate-50 font-bold p-6">The Sound of a New Generation</h1>
                 <p className="font-bold text-slate-50 text-xl p-6 ">Featuring active noise cancelling that gives you the space to create with fully immersive sound.</p>
-
                 <button className=" text-slate-50 rounded-md font-bold ml-5 p-2 bg-red-700">Buy Now</button>
             </div>
         </div>
+        <div className="mb-5">
+            <Reviews/>
+            <button className='text-slate-50 rounded-md font-bold ml-5 p-2 bg-red-700'>  
+            <Customlink to='/Reviews'>See All</Customlink></button>
+        </div>
+        
+        </section>
+        
     );
 };
 
